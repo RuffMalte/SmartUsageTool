6c9a9309beca35021a367c16a6d2f56391a5d404
MolodetsSerhii <s.molodets@devhive.fun>
2024-04-01 11:31:09 +0300

    create header view

--- SmartUsageTool/SmartUsageTool/Modules/Home/View/HomeView.swift
+++ SmartUsageTool/SmartUsageTool/Modules/Home/View/HomeView.swift
@@ -8,21 +8,54 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var text = ""
+    
     var body: some View {
-        VStack(alignment: .leading) {
+        contentView
+    }
+    
+}
+
+private extension HomeView {
+    var contentView: some View {
+        headerView
+    }
+    
+    var headerView: some View {
+        VStack(alignment: .leading, spacing: 20) {
             HStack {
                 Text("My Home")
                     .fontWeight(.bold)
                 Spacer()
                 Button(action: {}, label: {
-                   Image(systemName: "plus")
+                    Image(systemName: "plus")
+                        .tint(.black)
                 })
             }
             
-        Text("Total cost: $0,00")
-            Spacer()
+            Text("Total cost: $0,00")
+            
+            HStack {
+                Text("USD")
+                    .padding(10)
+                    .background(
+                                   RoundedRectangle(cornerRadius: 10)
+                                    .fill(.white)
+                               )
+                    .frame(height: 48)
+                TextField("0,00", text: $text)
+                        .padding(10)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .background(
+                                       RoundedRectangle(cornerRadius: 10)
+                                        .fill(.white)
+                                   )
+                Text("per kWh")
+            }
         }
         .padding()
+        .background(Color.background)
     }
 }
 
